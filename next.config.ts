272b434cba3f9304073e9b83cd8558b073aa3a0d
HyeonWooNa0861 <nahw0861@gmail.com>
2026-01-19 15:44:11 +0900

    BaePo:)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/proj_FollowSync',
+  images: {
+    unoptimized: true,
+  },
+  // 빌드 후 .nojekyll 자동 생성
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      const fs = require('fs');
+      const path = require('path');
+      fs.writeFileSync(path.join(__dirname, 'out', '.nojekyll'), '');
+    }
+    return config;
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
